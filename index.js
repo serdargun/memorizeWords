e034a8ae9c32c0cb716ba0d8882a7740c1f0d50a
Serdar <ahmetserdargun@gmail.com>
2023-09-17 14:53:41 +0300

    chore: implement admob lib

--- index.js
+++ index.js
@@ -5,5 +5,12 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {MobileAds} from 'react-native-google-mobile-ads';
+
+MobileAds()
+  .initialize()
+  .then(adapterStatuses => {
+    // Initialization complete!
+  });
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/constants/admob.ts
@@ -0,0 +1,6 @@
+const admob = {
+  banner_ios: 'ca-app-pub-8741759179035407/9599307435',
+  banner_android: 'ca-app-pub-8741759179035407/9630262872',
+};
+
+export default admob;
