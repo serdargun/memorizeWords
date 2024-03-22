e02f3c95b8a3baea79f7edb84a69e946e5e07eee
Serdar <ahmetserdargun@gmail.com>
2024-03-22 23:23:55 +0300

    chore: add request tracking permission package

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from './src/navigation';
+import {requestTrackingPermission} from 'react-native-tracking-transparency';
 
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      await requestTrackingPermission();
+    };
+
+    init();
+  }, []);
+
   return <Navigation />;
 };
 
